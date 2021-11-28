55ef8a40d585bf1b3b1652038b557e9e6f861ad7
Denis Putnam <denisputnam@verizon.net>
2021-11-28 12:54:57 -0500

    Initial checkin.

---
+++ src/test/java/com/ebusiness/niche/NicheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebusiness.niche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
